ea062e8b363695999320c57786a42ed4c5af3aa4
KJ Price <kjprice12@gmail.com>
2020-11-29 18:28:23 -0600

    Invert y-axis

--- src/sketch/drawPoint.js
+++ src/sketch/drawPoint.js
@@ -3,16 +3,22 @@ import {
     BOX_HEIGHT,
     middleX,
     middleY,
-    middleZ
+    middleZ,
+    CANVAS_HEIGHT
 } from '../settings/sketch';
 
 export default function drawPoint(point) {
     const { coordinates, isStarting, isEnding } = point;
     push();
     const [x, y, z] = coordinates;
+
+    // For p5.js sketches, the y value at the top of the sketch starts at 0
+    const invertedY = CANVAS_HEIGHT - y;
+
     const xDiff = x - middleX;
-    const yDiff = y - middleY;
+    const yDiff = invertedY - middleY;
     const zDiff = z - middleZ;
+
     if (isStarting) {
         fill('red');
     } else if (isEnding) {
