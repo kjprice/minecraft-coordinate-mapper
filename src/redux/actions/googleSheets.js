1ef84f74d567f8ca21fe905fc71680dc8aa8ec8d
KJ Price <kjprice12@gmail.com>
2022-11-26 11:18:33 -0600

    Prepare for url dropdowns

--- src/redux/actions/googleSheets.js
+++ src/redux/actions/googleSheets.js
@@ -1,6 +1,6 @@
-export const GOOGLE_SHEET_SET_SPREADSHEET_URL = 'GOOGLE_SHEET_SET_SPREADSHEET_URL';
+export const GOOGLE_SHEET_SET_EDIT_SPREADSHEET_URL = 'GOOGLE_SHEET_SET_EDIT_SPREADSHEET_URL';
 
-export const setSpreadsheetUrl = (spreadsheetUrl) => ({
-    type: GOOGLE_SHEET_SET_SPREADSHEET_URL,
-    spreadsheetUrl
+export const setEditSpreadsheetUrl = (editSpreadsheetUrl) => ({
+    type: GOOGLE_SHEET_SET_EDIT_SPREADSHEET_URL,
+    editSpreadsheetUrl
 });
