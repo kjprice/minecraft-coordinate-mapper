913ed7b0cad6234cf0f03b2ba0dcd4ed1613c9c5
KJ Price <kjprice12@gmail.com>
2022-11-28 23:58:48 -0600

    Remove unused code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import coordinates from './coordinates';
 import googleSheets from './googleSheets';
 
 export default combineReducers({
-    coordinates,
     googleSheets,
 });
